b9f30393a44a173e4e589a6b4ae0488d537768a6
ruqruqruq <lupinibrahim@gmail.com>
2023-04-12 23:17:45 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\TravelPackageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index'])
+        ->name('home');
+
+Route::get('/detail', [DetailController::class, 'index'])
+        ->name('detail');
+
+Route::get('/checkout', [CheckoutController::class, 'index'])
+        ->name('checkout');
+
+Route::get('/checkout/success', [CheckoutController::class, 'success'])
+        ->name('checkout-success');
+
+Route::prefix('admin')
+    ->middleware(['auth', 'admin'])
+    ->group(function() {
+        Route::get('/',[DashboardController::class, 'index'])
+            ->name('dashboard');
+
+        Route::resource('travel-package', TravelPackageController::class);
+        });
+
+
+// Route::get('/admin', [DashboardController::class, 'index'] );
+
+Auth::routes(['verify'=> true]);
+
